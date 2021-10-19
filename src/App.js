9e22f71f3123b855a86eb960e3753c2e5001ae7c
TS22082 <ts22082@gmail.com>
2021-10-18 19:54:10 -0700

    fun

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Products</Link>
+              </li>
+              <li>
+                <Link to="/product">Product</Link>
+              </li>
+              <li>
+                <Link to="/cart">cart</Link>
+              </li>
+              <li>
+                <Link to="/checkout">checkout</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route exact path="/">
+              <h1>products page</h1>
+            </Route>
+            <Route path="/product/:id ">
+              <h1>product page</h1>
+            </Route>
+            <Route path="/cart">
+              <h1>cart page page</h1>
+            </Route>
+            <Route path="/checkout">
+              <h1>checkout page</h1>
+            </Route>
+            <Route path="/">
+              <h1>404 page</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
