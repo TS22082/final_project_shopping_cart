08d377627fb838841246083aa1688eac91a8c78d
TS22082 <ts22082@gmail.com>
2021-10-20 19:29:04 -0700

    feat(products, product, item) create components and render to screen

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
+import CartContext from "../../Context/CartContext";
 
 const Cart = () => {
+  const { cart, setCart } = useContext(CartContext);
   return (
     <div>
       <h1>This is the cart page</h1>
